01ecf4269202c7712404be8ff6cf4931c1878729
HedroPedro <pedrohenrique.oliveira119@gmail.com>
2024-04-21 13:54:02 -0300

    A

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,7 +10,8 @@ class Slider {
         this.sourceArray = sourceArray;
     };
 
-    /** Função resposnável por alterar a imagem do slider por meio de um modificador, considerando que cada slider pode ser representado como um array do path das iamgens
+    /** Função resposnável por alterar a imagem do slider por meio de um modificador, 
+    * considerando que cada slider pode ser representado como um array do path das iamgens
     * @param {number} modifier
     * @return {string} o path da imagem para ser mostrada pelo slider
     */
